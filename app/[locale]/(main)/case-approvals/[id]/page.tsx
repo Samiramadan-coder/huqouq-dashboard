12dce73e17c8af6a60b251b4227d47b7f2cedb27
sami-ramadan <samiramadan389@gmail.com>
2026-09-01 20:14:14 +0300

    Add navigation link and loading spinner to case details page; update translations for back navigation

--- app/[locale]/(main)/case-approvals/[id]/page.tsx
+++ app/[locale]/(main)/case-approvals/[id]/page.tsx
@@ -1,13 +1,19 @@
 import CaseDetailsPreview from "@/components/case-approvals/case-details-preview";
+import { Spinner } from "@/components/ui/spinner";
+import { Link } from "@/i18n/navigation";
 import { http } from "@/lib/http";
 import { CaseDetails } from "@/types/case-approvals";
+import { ChevronLeft } from "lucide-react";
+import { getTranslations } from "next-intl/server";
+import { Suspense } from "react";
 
 type Params = {
   id: string;
 };
 
-export default async function Page({ params }: { params: Promise<Params> }) {
-  const { id } = await params;
+async function CaseDetailsInfo({ params }: { params: Params }) {
+  const t = await getTranslations("CaseApprovals.Details");
+  const { id } = params;
 
   const { data, ok } = await http.get<{ data: CaseDetails }>(
     `/api/admin/case-approvals/${id}`,
@@ -21,8 +27,27 @@ export default async function Page({ params }: { params: Promise<Params> }) {
   }
 
   return (
-    <div className="p-4 sm:p-6">
-      <CaseDetailsPreview caseDetails={data.data} />
+    <div>
+      <div className="p-4 sm:p-6 bg-white border-b border-gray-200">
+        <Link href="/lawyer-approvals" className="flex items-center gap-3">
+          <ChevronLeft className="rtl:rotate-180 size-4" />
+          <p className="text-[13px] text-gray-500">{t("backToPending")}</p>
+        </Link>
+      </div>
+
+      <div className="p-4 sm:p-6">
+        <CaseDetailsPreview caseDetails={data.data} />
+      </div>
     </div>
+  );
+}
+
+export default async function Page({ params }: { params: Promise<Params> }) {
+  return (
+    <Suspense
+      fallback={<Spinner className="h-10 w-10 text-primary m-4 sm:m-6" />}
+    >
+      <CaseDetailsInfo params={await params} />
+    </Suspense>
   );
 }
